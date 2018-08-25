39507380d27838f0f3e26dfe2304e11986f24cac
PawelBalawender <pbalaweneder101@gmail.com>
2018-08-25 18:24:07 +0200

    TCPL, Ex. 1-20

---
+++ detab.c
@@ -0,0 +1,65 @@
+/*
+"The C programming language", Ex. 1-20
+Write a program detab that replaces tabs in the input with the proper number
+of blanks to space to the next tab stop. Assume a fixed set of tab stops, say
+every n columns. Shuld n be a variable or a symbolic parameter?"
+*/
+#include<stdio.h>
+#define MAX_SIZE 1024
+#define TAB_STOP 4
+
+char* get_data(void);
+void detab(char* arr, char* container);
+
+/* Output the obtained by get_data() string with tabs expanded */
+int main(void) {
+    /* It's a test; exclamation mark should be in the same column in each of
+     * cases [0:4] and in each of [4:5]
+     */
+    char* data[6];
+    data[0] = "\t!";
+    data[1] = "1\t!";
+    data[2] = " 2\t!";
+    data[3] = "  3\t!";
+    data[4] = "   4\t!";
+    data[5] = "      7\t!";
+    char expanded[MAX_SIZE * TAB_STOP];
+    for (int i=0; i < 6; i++) {
+        detab(data[i], expanded);
+        printf("%s\n", expanded);
+    }
+    return 0;
+}
+
+/* Obtain the data: from a file, STDIN or a const like in this case */
+char* get_data(void) {
+    char* arr = "Hello,\tworld!\n";
+    return arr;
+}
+
+/* Change every tab in the input string to number of spaces sufficient to
+ * reach the next tab stop
+ */
+void detab(char* raw, char* container) {
+    // count holds the number of chars on the line to find the closest tabstop
+    int i, count=0;
+    while(*raw) {
+        // insert a proper amount of spaces and then decrement the pointer
+        // before it will be incremented again at the end of the loop
+        if (*raw == '\t') {
+            for (i=0; i < TAB_STOP - count % TAB_STOP; i++) {
+                *container = ' ';
+                container++;
+            }
+            container--;
+        } else {
+            if (*raw == '\n') count = 0;
+            *container = *raw;
+        }
+
+        count++;
+        raw++;
+        container++;
+    }
+}
+
