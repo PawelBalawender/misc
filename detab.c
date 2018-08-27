a845215fcf17b25a0ebf36fa2fa0df35be6b2a2a
PawelBalawender <pbalaweneder101@gmail.com>
2018-08-28 00:01:28 +0200

    resolved segfault

--- detab.c
+++ detab.c
@@ -1,68 +1,66 @@
-/*
-"The C programming language", Ex. 1-20
-Write a program detab that replaces tabs in the input with the proper number
-of blanks to space to the next tab stop. Assume a fixed set of tab stops, say
-every n columns. Shuld n be a variable or a symbolic parameter?"
-*/
+/* The C programming language, ex. 1-20
+ * Write a program detab that replaces tabs in the input with the proper number
+ * of blanks to space to the next tab stop. Assume a fixed set of tab stops, say
+ * every n columns.
+ */
 #include<stdio.h>
 #define MAX_SIZE 1024
 #define TAB_STOP 4
 
-static int i;
+char* detabbed(char*);
 
-char* get_data(void);
-void detab(char* arr, char* container);
-
-/* Output the obtained by get_data() string with tabs expanded */
+/* Output should read:
+ * "    !"
+ * "    !"
+ * "!   !"
+ * "!!! !"
+ * "!!!!!   !"
+ */
 int main(void) {
-    /* It's a test; exclamation mark should be in the same column in each of
-     * cases [0:4] and in each of [4:5]
-     */
-     
-    char expanded[MAX_SIZE * TAB_STOP];
-    char* data[6];
-    data[0] = "\t!";
-    data[1] = "1\t!";
-    data[2] = " 2\t!";
-    data[3] = "  3\t!";
-    data[4] = "   4\t!";
-    data[5] = "      7\t!";
-    for (i=0; i < 6; i++) {
-        detab(data[i], expanded);
-        printf("%s\n", expanded);
+    unsigned int i;
+    char* strings[5];
+    strings[0] = "    !";
+    strings[1] = "\t!";
+    strings[2] = "!\t!";
+    strings[3] = "!!!\t!";
+    strings[4] = "!!!!!\t!";
+    
+    for (i=0; i < 5; i++) {
+        printf("%s\n", detabbed(strings[i]));
     }
     return 0;
-}
-
-/* Obtain the data: from a file, STDIN or a const like in this case */
-char* get_data(void) {
-    char* arr = "Hello,\tworld!\n";
-    return arr;
 }
 
 /* Change every tab in the input string to number of spaces sufficient to
  * reach the next tab stop
  */
-void detab(char* raw, char* container) {
-    /* count holds the number of chars on the line to find the closest tabstop */
-    int count=0;
-    while(*raw) {
+char* detabbed(char* original) { 
+    unsigned int i;
+    static char container[MAX_SIZE];
+    char* c = container;
+    
+    /* number of chars on the line */
+    int count = 0;
+    
+    while(*original) {
         /* insert a proper amount of spaces */
-        if (*raw == '\t') {
+        if (*original == '\t') {
             for (i=0; i < TAB_STOP - count % TAB_STOP; i++) {
-                *container = ' ';
-                container++;
+                *c = ' ';
+                c++;
             }
             /* decrement to neutralize the incrementation below */
-            container--;
+            c--;
         } else {
-            if (*raw == '\n') count = 0;
-            *container = *raw;
+            if (*c == '\n') count = 0;
+            *c = *original;
         }
 
         count++;
-        raw++;
-        container++;
+        original++;
+        c++;
     }
+
+    return container;
 }
 
