537d8310c368a4425ba36c67984883c8ef88d255
Paweł <pbalawender101@gmail.com>
2018-09-11 16:28:37 +0200

    tool to fix names from weather api

---
+++ rename.py
@@ -0,0 +1,16 @@
+import glob
+import os
+
+
+if __name__ == '__main__':
+    if input('Modify D.weather directory? [Y/n]').lower() in {'', 'n'}:
+        assert 0
+
+    for filename in glob.glob('D:\weather\\*'):
+        base, name = os.path.split(filename)
+        name, ext = os.path.splitext(name)
+        new_name = name[:15] + ext
+        new = os.path.join(base, new_name)
+
+        os.rename(filename, new)
+
