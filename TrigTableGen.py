195d0518f8fdec6096a5a59beb60f872dbcd222f
Paweł <pbalawender101@gmail.com>
2018-10-18 19:59:15 +0200

    atan2 gen

--- TrigTableGen.py
+++ TrigTableGen.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import decimal
-
 import numpy as np
 
 SAMPLES_NUM = 256
-MAX_ROW = 16
 
-xs = np.linspace(0, np.pi / 2, SAMPLES_NUM)
-ys = np.sin(xs)
 
-np.set_printoptions(precision=7,
-                    formatter={'float': '{: 1.7f},'.format},
-                    linewidth=108,
-                    suppress=True)
+def gen_sin() -> np.ndarray:
+    xs = np.linspace(0, np.pi / 2, SAMPLES_NUM)
+    return np.sin(xs)
 
-print(ys)
+
+def gen_atan2() -> list:
+    xs = np.linspace(1, -1, SAMPLES_NUM // 2 + 1)  # +1 to make it evenly spaced, with 0
+    xs = np.concatenate((xs, xs[::-1][1:-1]))
+
+    ys = np.linspace(1, -1, SAMPLES_NUM // 2 + 1)
+    ys = np.concatenate((ys, ys[::-1][1:-1]))
+    ys = np.roll(ys, SAMPLES_NUM // 4)
+    assert len(ys) == len(xs) == SAMPLES_NUM
+
+    values = [np.arctan2(y, xs) for y in ys]
+    return values
+
+
+# np.set_printoptions(precision=7,
+#                     formatter={'float': '{: 1.7f},'.format},
+#                     linewidth=80-4,
+#                     suppress=True)
+
+atan2_table = gen_atan2()
+for i in atan2_table:
+    print(i)
