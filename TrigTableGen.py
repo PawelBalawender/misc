e4cf8eacd3d14340f637c4e1fc357a2c5aae6605
Pawel <pbalawender101@gmail.com>
2018-10-23 22:28:34 +0200

    gen atan2 1st quarter

--- TrigTableGen.py
+++ TrigTableGen.py
@@ -11,23 +11,17 @@ def gen_sin() -> np.ndarray:
 
 
 def gen_atan2() -> list:
-    xs = np.linspace(1, -1, SAMPLES_NUM // 2 + 1)  # +1 to make it evenly spaced, with 0
-    xs = np.concatenate((xs, xs[::-1][1:-1]))
-
-    ys = np.linspace(1, -1, SAMPLES_NUM // 2 + 1)
-    ys = np.concatenate((ys, ys[::-1][1:-1]))
-    ys = np.roll(ys, SAMPLES_NUM // 4)
-    assert len(ys) == len(xs) == SAMPLES_NUM
-
-    values = [np.arctan2(y, xs) for y in ys]
+    # +1 to include 0
+    xs = np.linspace(0, 1, SAMPLES_NUM + 1)
+    ys = (1 - xs**2) ** (1/2)
+    values = np.arctan2(ys, xs)
     return values
 
 
-# np.set_printoptions(precision=7,
-#                     formatter={'float': '{: 1.7f},'.format},
-#                     linewidth=80-4,
-#                     suppress=True)
+np.set_printoptions(precision=7,
+                    formatter={'float': '{: 1.7f},'.format},
+                    linewidth=80-4,
+                    suppress=True)
 
 atan2_table = gen_atan2()
-for i in atan2_table:
-    print(i)
+print(atan2_table)
