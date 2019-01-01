63b2a1d5b161a2109079863904f7b87c04c1b314
Pawel <pbalawender101@gmail.com>
2019-01-01 21:36:07 +0100

    Add cached recursive calculating

--- Fib.py
+++ Fib.py
@@ -2,14 +2,28 @@
 This module computes consecutive Fibonacci numbers
 with O(1) time and memory complexity
 """
+from functools import lru_cache
 import math
 
 
 def fib(n):
     lambd_1 = 2 / (math.sqrt(5) - 1)
     lambd_2 = 2 / (-math.sqrt(5) - 1)
-    return round((lambd_1 ** n - lambd_2 ** n) / math.sqrt(5))
+    return math.ceil((lambd_1 ** n - lambd_2 ** n) / math.sqrt(5))
 
-for i in range(10):
-    print(fib(i))
+
+@lru_cache(maxsize=128)
+def fib_rec(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    return fib_rec(n-2) + fib_rec(n-1)
+
+
+for i in range(128):
+    try:
+        assert fib(i) == fib_rec(i)
+    except AssertionError:
+        print(fib(i), fib_rec(i))
+        break
+    # print(fib(i), fib_rec(i))
 
