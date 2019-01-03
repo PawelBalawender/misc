8a726cece44d8f761749d5fc762e63f72fec2aff
Pawel <pbalawender101@gmail.com>
2019-01-03 21:45:13 +0100

    upate

--- Brainfuck/comp.py
+++ Brainfuck/comp.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
-with open('HelloWorld.b') as doc:
-    src = doc.read()
 
 cmds = '+-><.,[]'
+trans = {
+        '+': 'ldrb r1, [r0]\nadd r1, r1, #1\nstrb r1, [r0]\n',
+        '-': 'ldrb r1, [r0]\nsub r1, r1, #1\nstrb r1, [r0]\n',
+        '>': 'add r0, r0, #1\n',
+        '<': 'sub r0, r0, #1\n',
+        '.': 'bl puts\n',
+        ',': 'bl gets\n',
+        }
 
-doc = ''.join(i for i in src if i in cmds)
+with open('naive_hello.b') as doc:
+    src = doc.read()
 
-print(doc)
+src = ''.join(i for i in src if i in cmds)
+
+with open('libbf.s') as doc:
+    code = doc.read()
+
+code += '.globl main\n'
+code += 'main:\n'
+code += 'mov r0, arr_addr\n'
+
+for cmd in src:
+    if cmd in '[]':
+        continue
+
+    code += trans[cmd]
+
+with open('_out.s', 'w') as doc:
+    doc.write(code)
+
