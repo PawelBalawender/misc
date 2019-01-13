cde11c5549d63920ae8e0d8c85d76413d075f0c4
Pawel <pbalawender101@gmail.com>
2019-01-13 23:57:10 +0100

    Quick trig-functions

---
+++ STM32/_math.h
@@ -0,0 +1,18 @@
+/* This header contains math functions' headers for the kisaburo project
+ * author: Pawel Balawender
+ * contact: voilagrange@gmail.com
+ */
+#ifndef KISABURO_MATH_H
+#define KISABURO_MATH_H
+
+//#include<stdint.h>  // int8_t
+
+#define PI (3.1415926f)
+#define TAU (2U * PI)
+
+float read_sin(float);
+float sin_(float);
+float cos_(float);
+float atan2_(float, float);
+
+#endif  // KISABURO_MATH_H
